60d53e2be6f86794e4d31a90a98e816dce40faee
bertrandong <bertrandongchangheng@gmail.com>
2024-02-13 21:27:19 +0800

    Add mobile view support

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import skillsAnimation from "./assets/skillsAnimation.json";
 import contactAnimation from "./assets/contactAnimation.json";
 import upAnimation from "./assets/upAnimation.json";
 import { BsFillMoonStarsFill } from "react-icons/bs";
-import { AiFillLinkedin, AiFillInstagram, AiFillFacebook, AiFillGithub, AiFillFire } from "react-icons/ai";
+import { AiFillLinkedin, AiFillInstagram, AiFillFacebook, AiFillGithub, AiFillFire, AiOutlineClose } from "react-icons/ai";
 import { FaGraduationCap, FaReact, FaCss3Alt, FaHtml5, FaNodeJs, FaPython, FaJava, FaTelegram, FaWhatsapp, FaDiscord } from "react-icons/fa6";
 import { MdVolunteerActivism } from "react-icons/md"
 import { BiLogoTailwindCss } from "react-icons/bi";
@@ -15,25 +15,47 @@ import { IoIosMail } from "react-icons/io"
 import { BiLogoPostgresql } from "react-icons/bi";
 import Typewriter from "typewriter-effect";
 import { ContactForm } from "./ContactForm";
+import { FaBars } from "react-icons/fa";
 import "./App.css";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
     <div className={darkMode ? 'dark' : '' }>
       <main className="px-8 bg-slate-100 dark:bg-gray-900">
-        <nav className="py-10 mb-8 flex justify-between md:px-20 lg: px-40">
+        <header className="sticky top-0 z-50 bg-slate-100 dark:bg-gray-900">
+        <nav className="container z-auto py-10 mb-8 flex justify-between px-20 items-center">
+          <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
           <h1 className="text-3xl font-bold font-silkscreen dark:text-white"><a href="#home">BERTRAND</a></h1>
-          <ul className="flex space-x-5 items-center">
-            <li className="hover:text-gray-500 dark:text-white dark:hover:text-gray-300"><a href="#about">About</a></li>
-            <li className="hover:text-gray-500 dark:text-white dark:hover:text-gray-300"><a href="#experience">Experience</a></li>
-            <li className="hover:text-gray-500 dark:text-white dark:hover:text-gray-300"><a href="#portfolio">Portfolio</a></li>
-            <li className="hover:text-gray-500 dark:text-white dark:hover:text-gray-300"><a href="#contact">Contact</a></li>
-            <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="ml-6 cursor-pointer text-2xl dark:text-white"/></li>
-            <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md" href={require('./images/Ong Chang Heng Bertrand_CV.pdf')} target="_blank">Resume</a></li>
+          <button
+              className="text-black cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none dark:text-white"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <FaBars />
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center justify-center" +
+              (navbarOpen ? "flex" : " hidden")
+            }
+          >
+          <ul className="flex flex-col lg:flex-row list-none lg:ml-auto lg:space-x-5 text-center items-center">
+            <li className="hover:text-gray-500 dark:text-white dark:hover:text-gray-300 my-1"><a href="#about">About</a></li>
+            <li className="hover:text-gray-500 dark:text-white dark:hover:text-gray-300 my-1"><a href="#experience">Experience</a></li>
+            <li className="hover:text-gray-500 dark:text-white dark:hover:text-gray-300 my-1"><a href="#portfolio">Portfolio</a></li>
+            <li className="hover:text-gray-500 dark:text-white dark:hover:text-gray-300 my-1"><a href="#contact">Contact</a></li>
+            <li className="my-1"><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl dark:text-white"/></li>
+            <li className="my-3"><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md" href={require('./images/Ong Chang Heng Bertrand_CV.pdf')} target="_blank">Resume</a></li>
           </ul>
+          </div>
+          </div>
         </nav>
+        </header>
 
         <section className="min-h-screen px-10" id="home">
           <div className="text-center p-10">
@@ -81,8 +103,8 @@ function App() {
             <h3 className="text-3xl dark:text-white">About Me</h3>
           </div>
 
-          <div className="flex">
-            <div className="mr-28">
+          <div className="lg:flex">
+            <div className="lg:mr-28 max-md:pb-6">
               <p className="text-md py-3 leading-8 text-gray-800 dark:text-gray-300">Since young, I have been very active in the Math Olympiad scene, participating in various competitions. That was when I realised I enjoyed problem solving.</p>
               <p className="text-md py-3 leading-8 text-gray-800 dark:text-gray-300">Ever since I created my very first website, I have been obsessed with the idea of programming. I believe programming provides us with the ability to create solutions and solve real world problems.</p>
               <p className="text-md py-3 leading-8 text-gray-800 dark:text-gray-300">I am deeply passionate about fields like Software Engineering, Artificial Intelligence and Web Development, and hope to be able to turn this interest into solutions for people in the future.</p>
@@ -91,7 +113,6 @@ function App() {
             <div class>
               <img src={require('./images/aboutphoto.jpg')} className="w-special h-auto rounded-2xl" />
             </div>
-
           </div>
 
           <div className="lg:flex gap-10">
@@ -128,36 +149,34 @@ function App() {
             <h3 className="text-3xl dark:text-white">Experience</h3>
           </div>
 
-          <div className="flex">
-            <div className="w-1/2">
+          <div className="lg:flex">
+            <div className="lg:w-1/2">
               <h3 className="py-3 font-silkscreen text-xl dark:text-gray-300">Coding Languages</h3>
-              <div className="flex space-x-12">
-                <img src={require('./images/html-5.png')} className="py-3 w-20 h-24 hover:scale-105" />
-                <img src={require('./images/css-3.png')} className="py-3 w-20 h-24 hover:scale-105" />
-                <img src={require('./images/java.png')} className="py-3 w-20 h-24 hover:scale-105" />
-                <img src={require('./images/js.png')} className="py-3 w-20 h-24 hover:scale-105" />
-                <img src={require('./images/python.png')} className="py-3 w-20 h-24 hover:scale-105" />
-                <img src={require('./images/sql-server.png')} className="py-3 w-20 h-24 hover:scale-105" />
+              <div>
+                <img src={require('./images/html-5.png')} className="py-3 w-20 h-24 hover:scale-105 inline-block" />
+                <img src={require('./images/css-3.png')} className="py-3 w-20 h-24 hover:scale-105 inline-block" />
+                <img src={require('./images/java.png')} className="py-3 w-20 h-24 hover:scale-105 inline-block" />
+                <img src={require('./images/js.png')} className="py-3 w-20 h-24 hover:scale-105 inline-block mr-2" />
+                <img src={require('./images/python.png')} className="py-3 w-20 h-24 hover:scale-105 inline-block" />
+                <img src={require('./images/sql-server.png')} className="py-3 w-20 h-24 hover:scale-105 inline-block" />
               </div>
               <h3 className="py-3 font-silkscreen text-xl dark:text-gray-300">Frameworks</h3>
-              <div className="flex space-x-12">
-                <img src={require('./images/tailwind-css.png')} className="py-3 w-24 h-24 hover:scale-105" />
-                <img src={require('./images/reactjs.png')} className="py-3 w-20 h-24 hover:scale-105" />
-                <img src={require('./images/mongodb.png')} className="py-3 w-20 h-24 hover:scale-105" />
-                <img src={require('./images/nodejs.png')} className="py-3 w-64 h-24 hover:scale-105" />
-              </div>
-              <div className="flex space-x-12">
-                <img src={require('./images/expressjs.png')} className="py-3 w-32 h-24 hover:scale-105" />
-                <img src={require('./images/postgresql.png')} className="py-3 w-20 h-24 hover:scale-105" />
+              <div>
+                <img src={require('./images/tailwind-css.png')} className="py-3 w-24 h-24 hover:scale-105 inline-block" />
+                <img src={require('./images/reactjs.png')} className="py-3 w-20 h-24 hover:scale-105 inline-block" />
+                <img src={require('./images/mongodb.png')} className="py-3 w-20 h-24 hover:scale-105 inline-block" />
+                <img src={require('./images/nodejs.png')} className="py-3 w-64 h-24 hover:scale-105 inline-block" />
+                <img src={require('./images/expressjs.png')} className="py-3 w-32 h-24 hover:scale-105 inline-block" />
+                <img src={require('./images/postgresql.png')} className="py-3 w-20 h-28 hover:scale-105 inline-block" />
               </div>
               <h3 className="py-3 font-silkscreen text-xl dark:text-gray-300">Integrated Development Environments</h3>
-              <div className="flex space-x-12">
-                <img src={require('./images/vscode.png')} className="py-3 w-20 h-24 hover:scale-105" />
-                <img src={require('./images/intellij.png')} className="py-3 w-20 h-24 hover:scale-105" />
-                <img src={require('./images/vim.png')} className="py-3 w-20 h-24 hover:scale-105" />
+              <div>
+                <img src={require('./images/vscode.png')} className="py-3 w-20 h-24 hover:scale-105 inline-block" />
+                <img src={require('./images/intellij.png')} className="py-3 w-20 h-24 hover:scale-105 inline-block" />
+                <img src={require('./images/vim.png')} className="py-3 w-20 h-24 hover:scale-105 inline-block" />
               </div>
             </div>
-            <div className="w-1/2">
+            <div className="lg:w-1/2 mt-10">
               <Lottie animationData={skillsAnimation} className="w-5/6 mx-auto dark:text-white" />
             </div>
           </div>
@@ -282,8 +301,8 @@ function App() {
             <h3 className="text-3xl dark:text-white">My Porfolio</h3>
           </div>
 
-          <div className="flex gap-x-8">
-          <div className="lg:flex gap-10 w-1/3">
+          <div className="lg:flex gap-x-8">
+          <div className="flex lg:gap-10 lg:w-1/3">
             <div className="flex-1 bg-indigo-100 rounded-xl p-10 my-10 hover:bg-indigo-200 duration-500 cursor-pointer dark:bg-gray-700 dark:hover:bg-gray-800" onClick={() => window.open('https://bertrandong.wixsite.com/codersdigest', '_blank')}>
               <img src={require('./images/codersdigest.png')} className="rounded-xl h-44 mx-auto" />
               <h4 className="text-xl mt-5 font-semibold dark:text-white">Coder's Digest</h4>
@@ -291,7 +310,7 @@ function App() {
               <p className="text-gray-600 dark:text-gray-300">Built with Wix</p>
             </div>
           </div>
-          <div className="lg:flex gap-10 w-1/3">
+          <div className="flex lg:gap-10 lg:w-1/3">
             <div className="flex-1 bg-indigo-100 rounded-xl p-10 my-10 hover:bg-indigo-200 duration-500 cursor-pointer dark:bg-gray-700 dark:hover:bg-gray-800" onClick={() => window.open('https://pocketplanner.vercel.app/', '_blank')}>
               <img src={require('./images/pocketplanner.png')} className="rounded-xl h-44 mx-auto" />
               <h4 className="text-xl mt-5 font-semibold dark:text-white">PocketPlanner</h4>
@@ -299,11 +318,11 @@ function App() {
               <p className="text-gray-600 dark:text-gray-300">Built with MongoDB, ExpressJS, ReactJS and NodeJS</p>
             </div>
           </div>
-          <div className="lg:flex gap-10 w-1/3">
+          <div className="flex lg:gap-10 lg:w-1/3">
             <div className="flex-1 bg-indigo-100 rounded-xl p-10 my-10 hover:bg-indigo-200 duration-500 cursor-pointer dark:bg-gray-700 dark:hover:bg-gray-800" onClick={() => window.open('https://h4g.vercel.app/', '_blank')}>
               <img src={require('./images/h4g.png')} className="rounded-xl h-44 mx-auto" />
-              <h4 className="text-xl mt-5 font-semibold dark:text-white">Hack4Good Hackathon</h4>
-              <p className="text-gray-600 dark:text-gray-300">A fullstack web application to help the beneficiary track event participants and their attendance</p>
+              <h4 className="text-xl mt-5 font-semibold dark:text-white">Hack4Good Hackathon 2024</h4>
+              <p className="text-gray-600 dark:text-gray-300">A fullstack web application to help the beneficiary (Big At Heart) track event participants and their attendance</p>
               <p className="text-gray-600 dark:text-gray-300">Built with MongoDB, ExpressJS, ReactJS and NodeJS</p>
             </div>
           </div>
@@ -316,12 +335,12 @@ function App() {
             <h3 className="text-3xl dark:text-white">Contact</h3>
           </div>
               
-          <div className="flex">
-            <div className="w-1/2 mt-3">
+          <div className="lg:flex">
+            <div className="lg:w-1/2 mt-3">
               <p className="pb-5 dark:text-gray-300">I'm always open to talk about any enquiries or simply just chat about my past experiences and projects. Feel free to reach out to me on any of my social media platforms!</p>
               <ContactForm />
             </div>
-            <div className="w-1/2">
+            <div className="lg:w-1/2">
               <Lottie animationData={contactAnimation} className="w-5/6 mx-auto" />
             </div>
           </div>
